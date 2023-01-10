28c582fc39e11b1d67623fb988973055ed3c77e4
Jaejeong Sohn <jaejlf@naver.com>
2023-01-10 18:19:10 +0900

    setting: Health Check API 생성

--- src/chat/src/main/java/distove/chat/ChatApplication.java
+++ src/chat/src/main/java/distove/chat/ChatApplication.java
@@ -2,9 +2,7 @@ package distove.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ChatApplication {
 
