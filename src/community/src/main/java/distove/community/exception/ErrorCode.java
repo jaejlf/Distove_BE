741bc68f26e0a8b14bf22df647b440c1be1ca313
Jaejeong Sohn <jaejlf@naver.com>
2023-01-13 13:45:58 +0900

    add: 모든 서비스에 공통 모듈 import

---
+++ src/community/src/main/java/distove/community/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package distove.community.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    SAMPLE_ERROR(HttpStatus.NOT_FOUND, "X0001", "샘플 예외입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+}
+
