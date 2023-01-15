36fbdb920d9ce87343355761d8019c65e1ad4218
Jaejeong Sohn <jaejlf@naver.com>
2023-01-15 15:30:23 +0900

    add: 서버조회실패 에러코드 추가

--- src/community/src/main/java/distove/community/exception/ErrorCode.java
+++ src/community/src/main/java/distove/community/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    SAMPLE_ERROR(HttpStatus.NOT_FOUND, "X0001", "샘플 예외입니다.");
+    SAMPLE_ERROR(HttpStatus.NOT_FOUND, "X0001", "샘플 예외입니다."),
+    SERVER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "C0001","존재하지 않는 서버입니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
