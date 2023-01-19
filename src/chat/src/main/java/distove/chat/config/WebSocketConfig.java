79b694fb2b3eb5e7f130b1fabd768d77dba827d8
Jaejeong Sohn <jaejlf@naver.com>
2023-01-19 14:07:09 +0900

    add: STOMP CORS 설정

--- src/chat/src/main/java/distove/chat/config/WebSocketConfig.java
+++ src/chat/src/main/java/distove/chat/config/WebSocketConfig.java
@@ -18,7 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS(); // SockJS 활성화
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:3000")
+                .withSockJS(); // SockJS 활성화
     }
 
 }
