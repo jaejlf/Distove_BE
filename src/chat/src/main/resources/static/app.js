3069b1831b476b8b27f99d1bc8d2e69be07b389e
Jaejeong Sohn <jaejlf@naver.com>
2023-01-24 22:43:40 +0900

    update: 채팅 테스트 FE 페이지 업데이트

--- src/chat/src/main/resources/static/app.js
+++ src/chat/src/main/resources/static/app.js
@@ -1,57 +1,12 @@
 var stompClient = null;
 var wsLink = "/chat/ws";
 
-/**
- * publishMessage
- */
-// var subLink = "/sub/1";
-// var pubLink = "/pub/chat/1";
-// var inputData = {
-//     userId: 1,
-//     type: 'TEXT',
-//     content: 'Hello Hello~!~!'
-// }
-
-/**
- * mod & del
- */
-// var subLink = "/sub/1";
-// var pubLink = "/pub/chat/1";
-// var inputData = {
-//     userId: 1,
-//     type: 'MODIFIED',
-//     messageId: '63ce2a96ac2d4468f06c9971',
-//     content: '수정된 메시지'
-// }
-
-/**
- * beingTyped
- */
-// var subLink = "/sub/3";
-// var pubLink = "/pub/typing/3";
-// var inputData = {
-//     userId: 1
-// }
-
-/**
- * createReply
- */
-var subLink = "/sub/1";
-var pubLink = "/pub/reply/1";
-var inputData = {
-    userId: 1,
-    type: 'TEXT',
-    content: 'repl-repl-repl',
-    parentId: "63ce2a96ac2d4468f06c9971"
-}
-
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
         $("#conversation").show();
-    }
-    else {
+    } else {
         $("#conversation").hide();
     }
 }
@@ -62,7 +17,7 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe(subLink, function (greeting) {
+        stompClient.subscribe($("#sub").val(), function (greeting) {
         });
     });
 }
@@ -76,14 +31,27 @@ function disconnect() {
 }
 
 function sendName() {
-    stompClient.send(pubLink, {}, JSON.stringify(inputData));
+    stompClient.send($("#pub").val(), {'userId': $("#userId").val()}, JSON.stringify(
+        {
+            'type': $("#type").val(),
+            'content': $("#content").val(),
+            'messageId': $("#messageId").val(),
+            'parentId': $("#parentId").val()
+        }
+    ));
 }
 
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $("#connect").click(function () {
+        connect();
+    });
+    $("#disconnect").click(function () {
+        disconnect();
+    });
+    $("#send").click(function () {
+        sendName();
+    });
 });
