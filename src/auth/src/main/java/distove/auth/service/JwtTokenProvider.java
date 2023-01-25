a9e3986705eb057f8a1be6b88fd559e45a07de4d
Jaejeong Sohn <jaejlf@naver.com>
2023-01-26 00:10:19 +0900

    feat: 토큰 검증 기능

--- src/auth/src/main/java/distove/auth/service/JwtTokenProvider.java
+++ src/auth/src/main/java/distove/auth/service/JwtTokenProvider.java
@@ -1,17 +1,21 @@
 package distove.auth.service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.time.Duration;
 import java.util.Date;
-
+import java.util.HashMap;
+import java.util.Map;
+@Slf4j
 @Component
 public class JwtTokenProvider {
 
@@ -23,9 +27,14 @@ public class JwtTokenProvider {
     }
 
     public String generateAccessToken(String email) {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
         Claims claims = Jwts.claims().setSubject(email);
         Date now = new Date();
         return Jwts.builder()
+                .setHeader(headers)
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis()))
@@ -37,9 +46,21 @@ public class JwtTokenProvider {
         Claims claims = Jwts.claims().setSubject(email);
         Date now = new Date();
         return Jwts.builder()
-                .setClaims(claims)
                 .setExpiration(new Date(now.getTime() + Duration.ofDays(30).toMillis()))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean validateToken(String jwt) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt);
+            return true;
+        } catch (JwtException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
