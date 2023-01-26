db6432e86f68df7ffcd2035ac5cb74b727917270
Jaejeong Sohn <jaejlf@naver.com>
2023-01-26 16:08:09 +0900

    update: 엔티티 수정

--- src/community/src/main/java/distove/community/entity/Member.java
+++ src/community/src/main/java/distove/community/entity/Member.java
@@ -12,7 +12,6 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
     @ManyToOne//(fetch = FetchType.LAZY)
     @JoinColumn(name ="server_id")
     public Server server;
