eb84f18b7ec232e995b4512cae9203539c5ba4b7
Jaejeong Sohn <jaejlf@naver.com>
2023-01-26 16:06:43 +0900

    feat: 서버 생성, 이미지 추가 api 완료

--- src/community/src/main/java/distove/community/controller/ServerController.java
+++ src/community/src/main/java/distove/community/controller/ServerController.java
@@ -1,14 +1,17 @@
 package distove.community.controller;
 
-import distove.common.ResultResponse;
-import distove.community.dto.request.ServerRequest;
-import distove.community.dto.response.ServerResponse;
+import distove.community.dto.response.CategoryResponse;
+import distove.community.dto.response.ResultResponse;
 import distove.community.entity.Server;
+import distove.community.service.CategoryService;
 import distove.community.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,22 +19,27 @@ public class ServerController {
 
     private final ServerService serverService;
 
+    private final CategoryService categoryService;
+    @GetMapping("/server")
+    public ResponseEntity<Object> getServersByUserId(@RequestHeader("userId") Long userId){
+        List<Server> servers = serverService.getServersByUserId(userId);
+        return ResultResponse.success(HttpStatus.OK,"서버 리스트 조회(By user)",servers);
+    }
 
-//    @GetMapping("/server")
-//    public ResponseEntity<Object> getServersByUserId(@RequestHeader("userId") Long userId){
-//        List<Member> servers = serverService.getServersByUserId(userId);
-//        return ResultResponse.success(HttpStatus.OK,"전체 그룹의 채널 및 카테고리 조회",servers);
-//    }
+    @GetMapping("/server/{serverId}")
+    public ResponseEntity<Object> getServersByUserId(@RequestHeader("userId") Long userId,
+                                                     @PathVariable("serverId") Long serverId){
+        List<CategoryResponse> categories = categoryService.getCategoriesWithChannelsByServerId(serverId);
+        return ResultResponse.success(HttpStatus.OK,"그룹의 채널 및 카테고리 조회",categories);
+    }
 
     @PostMapping("/server")
-    public Server createNewServer(@RequestHeader("userId") Long userId,
-                                  @RequestBody ServerRequest serverRequest){
-        return serverService.createNewServer(serverRequest);
+    public ResponseEntity<Object> createNewServer(@RequestHeader("userId") Long userId,
+                                                  @RequestPart("name") String name,
+                                                  @RequestPart("image") MultipartFile image){
+        Server server =  serverService.createNewServer(name,image);
+        return ResultResponse.success(HttpStatus.OK,"서버 생성 성공",server);
     }
 
-    @GetMapping("/server")
-    public ResponseEntity<Object> getServerById(@RequestHeader("serverId") Long serverId){
-        ServerResponse serverResponse = serverService.getServerById(serverId);
-        return ResultResponse.success(HttpStatus.OK,"그룹의 채널 및 카테고리 조회",serverResponse);
-    }
+
 }
