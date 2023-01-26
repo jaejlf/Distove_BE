beff792855e67f665c5970f9117a48c08f0f57ee
Jaejeong Sohn <jaejlf@naver.com>
2023-01-26 16:07:21 +0900

    update: ServerDto 업데이트

--- src/community/src/main/java/distove/community/dto/response/ServerDto.java
+++ src/community/src/main/java/distove/community/dto/response/ServerDto.java
@@ -1,22 +1,14 @@
 package distove.community.dto.response;
 
-import distove.community.entity.Category;
-import distove.community.entity.Channel;
 import lombok.Getter;
-
-import java.util.List;
 @Getter
 public class ServerDto {
     private Long id;
     private String name;
-    private List<Category> categories;
-    private List<Channel> channels;
 
-
-    public ServerDto(Long id, String name, List<Category> categories, List<Channel> channels){
+    public ServerDto(Long id, String name){
         this.id = id;
         this.name = name;
-        this.categories = categories;
-        this.channels = channels;
+
     }
 }
