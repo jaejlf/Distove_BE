292d67f30578b8cb640576ed78f177856ca44187
Jaejeong Sohn <jaejlf@naver.com>
2023-01-27 10:07:20 +0900

    feat: 첫 연결 시 welcome 메시지 생성

--- src/chat/src/main/java/distove/chat/service/ConnectionService.java
+++ src/chat/src/main/java/distove/chat/service/ConnectionService.java
@@ -2,6 +2,9 @@ package distove.chat.service;
 
 import distove.chat.entity.Connection;
 import distove.chat.repository.ConnectionRepository;
+import distove.chat.repository.MessageRepository;
+import distove.chat.web.UserClient;
+import distove.chat.web.UserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,13 +13,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static distove.chat.entity.Connection.*;
+import static distove.chat.entity.Message.newMessage;
+import static distove.chat.enumerate.MessageType.WELCOME;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ConnectionService {
 
+    private final UserClient userClient;
     private final ConnectionRepository connectionRepository;
+    private final MessageRepository messageRepository;
 
     public void createConnection(Long channelId, Long userId) {
         List<Long> connectedMemberIds = new ArrayList<>();
@@ -24,6 +31,9 @@ public class ConnectionService {
 
         Connection connection = newConnection(channelId,connectedMemberIds);
         connectionRepository.save(connection);
+
+        UserResponse writer = userClient.getUser(userId);
+        messageRepository.save(newMessage(channelId, userId, WELCOME, writer.getNickname()));
     }
 
 }
