516b17df260a372f9e495a4c56c615ff85cce7e1
Jaejeong Sohn <jaejlf@naver.com>
2023-01-31 15:28:52 +0900

    update: 네이밍 변경

--- src/auth/src/main/java/distove/auth/config/StorageConfig.java
+++ src/auth/src/main/java/distove/auth/config/StorageConfig.java
@@ -17,6 +17,7 @@ public class StorageConfig {
 
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
+
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
