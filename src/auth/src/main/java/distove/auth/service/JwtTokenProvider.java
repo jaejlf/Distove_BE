babe45c8467a9628190606ce7407b65647f3c885
Jaejeong Sohn <jaejlf@naver.com>
2023-02-02 16:09:32 +0900

    update: 헤더 토큰 변경 및 로직 수정

--- src/auth/src/main/java/distove/auth/service/JwtTokenProvider.java
+++ src/auth/src/main/java/distove/auth/service/JwtTokenProvider.java
@@ -1,13 +1,14 @@
 package distove.auth.service;
 
-import distove.auth.dto.response.TokenResponse;
 import distove.auth.exception.DistoveException;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.time.Duration;
@@ -19,11 +20,13 @@ import static distove.auth.exception.ErrorCode.JWT_EXPIRED;
 import static distove.auth.exception.ErrorCode.JWT_INVALID;
 
 @Slf4j
-@Component
+@Service
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
     private final Key key;
 
+    @Autowired
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
@@ -42,7 +45,7 @@ public class JwtTokenProvider {
                 .setHeader(headers)
                 .setClaims(claims)
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(0).toMillis()))
+                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(43200).toMillis()))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
@@ -66,12 +69,6 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    /**
-     * 토큰 검증
-     * - 헤더의 토큰을 받아 토큰을 sign한 key와 expire되었는지 검증하는 메서드
-     * 피드백 받고 싶은 부분
-     * - 토큰 검증 과정에서 try-catch 구문을 많이 사용하게 되는데 예외 처리하는 최선의 방법일지 궁금합니다.
-     */
     public boolean validToken(String token) {
         try {
             Jwts.parserBuilder()
@@ -84,10 +81,6 @@ public class JwtTokenProvider {
         } catch (ExpiredJwtException e) {
             throw new DistoveException(JWT_EXPIRED);
         }
-    }
-
-    public TokenResponse reissue(String token) {
-        return TokenResponse.of(generateAccessToken(getUserId(token)), token);
     }
 
     public Long getUserId(String token) throws DistoveException {
@@ -105,4 +98,5 @@ public class JwtTokenProvider {
             throw new DistoveException(JWT_EXPIRED);
         }
     }
-}
+
+    }
