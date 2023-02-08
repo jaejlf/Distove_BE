23c092576576eccf5ac541442c18113a4b4122eb
Jaejeong Sohn <jaejlf@naver.com>
2023-02-08 14:26:37 +0900

    hotfix: 에러 코드 수정

--- src/chat/src/main/java/distove/chat/exception/ErrorCode.java
+++ src/chat/src/main/java/distove/chat/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     EVENT_HANDLE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S0000", "이벤트 처리에 실패했습니다."),
-    MESSAGE_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "S0001", "존재하지 않는 메시지입니다."),
+    MESSAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "S0001", "존재하지 않는 메시지입니다."),
     MESSAGE_TYPE_ERROR(HttpStatus.BAD_REQUEST, "S0002", "잘못된 메시지 타입입니다."),
     NO_AUTH(HttpStatus.BAD_REQUEST, "S0003", "수정/삭제 권한이 없습니다."),
     FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S0004", "파일 업로드에 실패했습니다."),
