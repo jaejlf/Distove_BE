67640f77546ec84a890b1d2bb0f7d7bc6c14bb3d
Jaejeong Sohn <jaejlf@naver.com>
2023-02-09 17:17:55 +0900

    feat: reaction get에서 조회, status 수정

--- src/chat/src/main/java/distove/chat/dto/response/MessageResponse.java
+++ src/chat/src/main/java/distove/chat/dto/response/MessageResponse.java
@@ -2,12 +2,14 @@ package distove.chat.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import distove.chat.entity.Message;
+import distove.chat.entity.Reaction;
 import distove.chat.enumerate.MessageType;
 import distove.chat.web.UserResponse;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 import static distove.chat.enumerate.MessageType.*;
@@ -25,8 +27,9 @@ public class MessageResponse {
     private UserResponse writer;
     private Boolean hasAuthorized;
     private ReplyInfoResponse replyInfo;
+    private List<ReactionResponse> reactions;
 
-    public static MessageResponse ofDefault(Message message, UserResponse writer, Long userId) {
+    public static MessageResponse ofDefault(Message message, UserResponse writer, Long userId,List<ReactionResponse> reactions) {
         return MessageResponse.builder()
                 .id(message.getId())
                 .type(message.getType())
@@ -35,10 +38,11 @@ public class MessageResponse {
                 .createdAt(message.getCreatedAt())
                 .writer(writer)
                 .hasAuthorized(Objects.equals(writer.getId(), userId))
+                .reactions(reactions)
                 .build();
     }
 
-    public static MessageResponse ofParent(Message message, UserResponse writer, Long userId, ReplyInfoResponse replyInfo) {
+    public static MessageResponse ofParent(Message message, UserResponse writer, Long userId, ReplyInfoResponse replyInfo,List<ReactionResponse> reactions) {
         return MessageResponse.builder()
                 .id(message.getId())
                 .type(message.getType())
@@ -48,6 +52,7 @@ public class MessageResponse {
                 .writer(writer)
                 .replyInfo(replyInfo)
                 .hasAuthorized(Objects.equals(writer.getId(), userId))
+                .reactions(reactions)
                 .build();
     }
 
