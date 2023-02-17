f5439f271a0e966cd0fa3ae82a5f8cfcee8694da
Jaejeong Sohn <jaejlf@naver.com>
2023-02-17 18:55:53 +0900

    update: event 업데이트

--- src/presence/src/main/java/distove/presence/config/EventConfig.java
+++ src/presence/src/main/java/distove/presence/config/EventConfig.java
@@ -4,13 +4,15 @@ import distove.presence.event.UpdateUserPresenceEvent;
 import distove.presence.process.EventConsumer;
 import distove.presence.service.EventService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import static distove.presence.enumerate.EventTopic.getEventQ;
 
+@Configuration
 public class EventConfig {
     @Bean
-    public EventConsumer<UpdateUserPresenceEvent> newChannelEventEventConsumer(EventService eventService) {
-        return new EventConsumer<>(getEventQ(UpdateUserPresenceEvent.class), eventService::runUpdateUserPresenceEvent);
+    public EventConsumer<UpdateUserPresenceEvent> UpdateUserPresenceEventConsumer(EventService eventService) {
+        return new EventConsumer<>(getEventQ(UpdateUserPresenceEvent.class), eventService::runUpdateUserPresence);
     }
 
 }
