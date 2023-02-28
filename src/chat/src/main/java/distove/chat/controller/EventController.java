ff343625d57bdd643e8df5926d3beae09773dc98
Jaejeong Sohn <jaejlf@naver.com>
2023-02-28 20:18:13 +0900

    update: 성능 측정을 위해 EventQ 사용하지 않도록

--- src/chat/src/main/java/distove/chat/controller/EventController.java
+++ src/chat/src/main/java/distove/chat/controller/EventController.java
@@ -2,6 +2,7 @@ package distove.chat.controller;
 
 import distove.chat.service.ConnectionService;
 import distove.chat.service.EventService;
+import distove.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ public class EventController {
 
     private final EventService eventService;
     private final ConnectionService connectionService;
+    private final MessageService messageService;
 
     @PostMapping("/web/connection/server/{serverId}")
     public void createConnection(@PathVariable Long serverId,
@@ -22,12 +24,22 @@ public class EventController {
 
     @DeleteMapping("/web/clear/{channelId}")
     public void clear(@PathVariable Long channelId) {
-        eventService.requestDelChannel(channelId);
+
+//        eventService.requestDelChannel(channelId);
+
+        connectionService.clear(channelId);
+        messageService.clear(channelId);
     }
 
     @DeleteMapping("/web/clear/list")
     public void clearAll(@RequestParam List<Long> channelIds) {
+
         eventService.requestDelChannelList(channelIds);
+    }
+
+    @GetMapping("/web/hello")
+    public String helloWeb() {
+        return "DEV SERVER 배포되었는나요?";
     }
 
 }
