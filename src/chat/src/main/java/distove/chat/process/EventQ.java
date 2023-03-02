24710f04cc69410a28f7248e138d5629e4e31ae8
Jaejeong Sohn <jaejlf@naver.com>
2023-03-02 10:11:05 +0900

    hotfix: merge되지 않은 코드

--- src/chat/src/main/java/distove/chat/process/EventQ.java
+++ src/chat/src/main/java/distove/chat/process/EventQ.java
@@ -3,13 +3,13 @@ package distove.chat.process;
 import distove.chat.event.Event;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Slf4j
 public class EventQ<T extends Event> {
 
-    private final Queue<T> queue = new LinkedList<>();
+    private final Queue<T> queue = new LinkedBlockingQueue<>();
 
     public void add(T event) {
         queue.offer(event);
