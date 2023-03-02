2c97cf15ad598b71bd97dcf520bbee453dd61a15
Jaejeong Sohn <jaejlf@naver.com>
2023-03-02 11:36:59 +0900

    refacor: 에러 코드 업데이트

--- src/voice/src/main/java/distove/voice/exception/ErrorCode.java
+++ src/voice/src/main/java/distove/voice/exception/ErrorCode.java
@@ -8,10 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    SAMPLE_ERROR(HttpStatus.NOT_FOUND, "X0001", "샘플 예외입니다."),
-    CHANNEL_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "V0002", "채널 없음"),
-    PARTICIPANT_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "V0003", "방에 있지 않은 유저"),
-    ROOM_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "V0004", "방 없음");
+    PARTICIPANT_NOT_FOUND(HttpStatus.BAD_REQUEST, "V0001", "방에 존재하지 않는 유저입니다."),
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "V0002", "방이 존재하지 않습니다.");
+
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
